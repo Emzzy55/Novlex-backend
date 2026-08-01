0f44b8a4d779ab894da75c17abca0d7ddf60016f
Emzzy55 <goldemzzy@gmail.com>
2026-08-01 21:11:52 +0100

    Update investment.js

--- routes/investment.js
+++ routes/investment.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getPlans, invest, getMyInvestments } = require('../controllers/investmentController');
+const { getPlans, invest, getMyInvestments, claimEarnings, getClaimStatus } = require('../controllers/investmentController');
 const { protect } = require('../middleware/auth');
 
 router.use(protect);
 router.get('/plans', getPlans);
 router.post('/invest', invest);
 router.get('/mine', getMyInvestments);
+router.post('/claim', claimEarnings);
+router.get('/claim-status', getClaimStatus);
 
 module.exports = router;
